eac01a303abbcb7e67d009f9c86a03b50b61f423
jjxy1213 <304125480@qq.com>
2016-09-27 16:55:48 +0800

    Update publicKey.js

--- src/util/publicKey.js
+++ src/util/publicKey.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var keystr = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIyirbChVxQFk3gItINV0ivasC2MpE1OzFzwgLt2nv14LXJTRmawLf1cduRhVWT13ldhidL601KE23Wabo30TKNJmMR0gLPD2PTq5JjmuwxSEd5AIdGm3OIaRrScQ24PlEbho2+ApTLjzCknGkY1wIDAQAB'
+var keystr = 'MIGfMA0GCSqGQKBgQCIyirbChVxQFk3gItINV0ivasC2MpE1OzFzwgLt2nv14LXJTRmawLf1cduRhVWT13ldhidL6NJmMR0gLPD2PTq5JjmuwxSEd5AIdGm3OIaRrScQ24PlEbho2+ApTLjzCknGkY1wIDAQAB'
 var encrypt = new JSEncrypt();
 encrypt.setPublicKey(keystr);
 
